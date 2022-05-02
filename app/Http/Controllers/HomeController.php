a0e3e2b5566350aacc5ee124147545795e7a396b
Dayanna <dayannalaitonp@gmail.com>
2022-05-01 20:37:40 -0500

    filtro

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,9 +37,6 @@ class HomeController extends Controller
         $categorias=DB::select('SELECT count(*) as c FROM categorias ');
         $productos=DB::select('SELECT count(*) as c FROM productos ');
         $clientes=DB::select('SELECT count(*) as c FROM clientes ');
-        $roles = Role::all();
-        return view('home', compact('usuarios','compras','proveedores','categorias','productos','clientes', 'roles'));
-
-
+        return view('home', compact('usuarios','compras','proveedores','categorias','productos','clientes'));
     }
 }
